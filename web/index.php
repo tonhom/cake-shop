b458e6a40f2234fdb08f3c35625f916628b0f747
tonhom <tonhom@ZeroX>
2015-02-11 21:14:06 +0700

    initial project

---
+++ web/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>ร้านต้นเค้ก</title>
+        <link type="text/css" rel="stylesheet" href="asset/css/materialize.min.css"  media="screen,projection"/>
+        <link type="text/css" rel="stylesheet" href="asset/css/theme.css"  media="screen,projection"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+        <script type="text/javascript" src="asset/js/jquery-2.1.3.min.js"></script>
+        <script type="text/javascript" src="asset/js/materialize.min.js"></script>
+    </head>
+
+    <body>
+        <nav>
+            <div class="nav-wrapper">
+                <a href="#" class="brand-logo" style="text-indent: 20px;">ร้านต้นเค้ก</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down top-navigation">
+                    <li>
+                        <a href="sass.html">
+                            หน้าร้าน
+                        </a>
+                    </li>
+                    <li>
+                        <a href="components.html">สั่งทำเค้ก</a>
+                    </li>
+                    <li>
+                        <a href="javascript.html">ติดต่อเรา</a>
+                    </li>
+                </ul>
+                <!-- Include this line below -->
+                <a class="button-collapse" href="#" data-activates="nav-mobile"><i class="mdi-navigation-menu"></i></a>
+                <!-- End -->
+            </div>
+        </nav>
+    </body>
+</html>
