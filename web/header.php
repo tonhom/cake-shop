abe74f0c67a4878e9bfe9d0c9a04dfac65f026f5
tonhom <tonhom@ZeroX>
2015-02-17 22:01:30 +0700

    first version used materialize

---
+++ web/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Freedom Cake</title>
+        <!--<link type="text/css" rel="stylesheet" href="asset/css/materialize.min.css"  media="screen,projection"/>-->
+        <!--<link type="text/css" rel="stylesheet" href="asset/css/theme.css"  media="screen,projection"/>-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+        <script type="text/javascript" src="asset/js/jquery-2.1.3.min.js"></script>
+        <script src="asset/js/velocity.min.js" type="text/javascript"></script>
+        <script src="asset/js/moment-with-locales.js" type="text/javascript"></script>
+        <script src="asset/js/angular.min.js" type="text/javascript"></script>
+        <script src="asset/js/lumx.min.js" type="text/javascript"></script>
+        <!--<script type="text/javascript" src="asset/js/materialize.min.js"></script>-->
+        <!--<script type="text/javascript" src="asset/js/jsHelper.js"></script>-->
+        
+    </head>
+    <body>
+
